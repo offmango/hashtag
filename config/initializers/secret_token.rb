d42059c3687ef417957c25acd708e7365099a591
Eric Kurzenberger <offmango@ETK-MacBook-Air.local>
2012-07-17 10:06:45 -0400

    added project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashtag::Application.config.secret_token = 'ca20dba5563e563cf5852b265f96b6537488a215d46be5619b91feb9bf79d9db8a2c1ed4cf13def90fc33e4d65272d82687648ac4fb0e5ecfeff42c0bcaac488'
