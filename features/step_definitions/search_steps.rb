4ad44478fae33053c6093aa6698290055b07587e
Eric Kurzenberger <offmango@ETK-MacBook-Air.local>
2012-07-17 15:35:44 -0400

    tweets now display from author

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -19,8 +19,9 @@ Given /^Twitter returns the following tweets when searching for "(.*?)":$/ do |h
 	Searcher.search_implementation = fake_twitter
 end
 
-Then /^I should see the tweet "(.*?)"$/ do |tweet_text|
+Then /^I should see the tweet "(.*?)" authored by "(.*?)"$/ do |tweet_text, tweet_from_user|
 	within '[data-role="search-results"]' do
 		page.should have_css("li:contains('#{tweet_text}')")
+		page.should have_css("li:contains('#{tweet_from_user}')")
 	end
 end
