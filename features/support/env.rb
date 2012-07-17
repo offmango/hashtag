f8f5fb01092cff1f7858a2414dda96a07ac480fc
Eric Kurzenberger <offmango@ETK-MacBook-Air.local>
2012-07-17 10:22:34 -0400

    configured gems

---
+++ features/support/env.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+	require 'cucumber/rails'
+	require 'capybara/rspec'
+	Capybara.default_selector = :css
+	ActionController::Base.allow_rescue = false
+end
+
+Spork.each_run do
+end
+
+
